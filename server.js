7cb77476f4143fc80f8cfe80dbd2d5484c636be2
salimalisalim <salimalisalim@gmail.com>
2024-03-23 18:31:25 +0530

    Removed app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const dotenv = require("dotenv");
-const app = require("./app");
+const express = require("express");
+const app = express();
+
 
 // dotenv.config({path:"./config/config.env"});
 
 
 // const restaurantRoutes = require("./routes/restaurantRoutes");
 
+app.use(express.json());
 
 app.get("/api/v1", (req,res)=>{
     res.status(200).json({
